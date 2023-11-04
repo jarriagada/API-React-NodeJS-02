328eec16a06d2af36f97158a56e4434ebe612cf9
jarriagada <jlas1680gmail.com>
2023-11-04 12:59:10 -0300

    feat: servidor node creado

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const {conexion} = require("./basedatos/conexion")
+const express = require("express");
+const cors = require("cors");
 
 //Inicia APP
 console.log("App node Arrancada")
 
 //conexion a base de datos
 conexion();
+
+//Crear servidor NodeJS
+const app = express();
+const puerto = 3900;
+//Configurar cors, se usa el midleware del cors, se ejecuta antes
+//evita errores de rutas cruzadas
+app.use(cors());
+
+//Leer y convertir el body en un objeto javascript
+//para tener objeto js usable en el codigo, queda parseado
+app.use(express.json());
+
+//crear rutas
+
+//crear servidor y escuchar (get) , peticiones (post) http
+//se le pasa un puerto como parametro para escuchar
+app.listen(puerto, () => {
+    console.log("servicio escuchando en el puerto : " + puerto)
+})
