738a3e29000410adec879f29b2e8c267c1dfa206
jarriagada <jlas1680gmail.com>
2023-11-04 13:23:03 -0300

    feat: add primera ruta get de prueba

--- index.js
+++ index.js
@@ -19,7 +19,28 @@ app.use(cors());
 //para tener objeto js usable en el codigo, queda parseado
 app.use(express.json());
 
-//crear rutas
+//crear rutas, con mongoose 6.4.3 con funcion de call-back
+app.get("/ruta-prueba", (req, res) => {
+
+    console.log("se ha ejecutado el EndPoint ruta-prueba")
+
+    return res.status(200).json([
+        {
+            curso: "node JS",
+            autor: "VR",
+            url: "jarriagada.cl"
+        },
+        {
+            curso: "node React",
+            autor: "VR",
+            url: "jarriagada.cl"
+        }
+
+    ]
+    );
+     
+
+} )
 
 //crear servidor y escuchar (get) , peticiones (post) http
 //se le pasa un puerto como parametro para escuchar
